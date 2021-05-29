4215a3d3d00b5697c6ab243c69cccf0fe7dfddd9
gabrielbar <gabrieel.barreto@gmail.com>
2021-05-29 19:41:02 -0300

    - Atualização de nomes - Inclusão de novos testes

---
+++ Calculator.Test/CalculetorTests.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Calculator.Library.Tests
+{
+    [TestClass]
+    public class CalculetorTests
+    {
+        [TestMethod]
+        public void Divide_PositiveNumber_ReturnsPositiveQuociente()
+        {
+            //Arrange
+            var ValorEsperado = 5;
+            var Numerador = 20;
+            var Denominador = 4;
+
+            //Act
+            var ValorAtual = Calcularora.Divide(Numerador, Denominador);
+
+            //Assert
+            Assert.AreEqual(ValorEsperado, ValorAtual);
+        }
+        [TestMethod]
+        public void Divide_PositiveNumeratorAndNegativeDenominator_ReturnsNegativeQuociente()
+        {
+            //Arrange
+            var ValorEsperado = -5;
+            var Numerador = 20;
+            var Denominador = -4;
+
+            //Act
+            var ValorAtual = Calcularora.Divide(Numerador, Denominador);
+
+            //Assert
+            Assert.AreEqual(ValorEsperado, ValorAtual);
+        }
+
+        [TestMethod]
+        public void Divide_NegativeNumbers_ReturnsPositiveQuociente()
+        {
+            //Arrange
+            var ValorEsperado = 5;
+            var Numerador = -20;
+            var Denominador = -4;
+
+            //Act
+            var ValorAtual = Calcularora.Divide(Numerador, Denominador);
+
+            //Assert
+            Assert.AreEqual(ValorEsperado, ValorAtual);
+        }
+    }
+}
